39009b21948b2b5ba4bb7e59fab0bce7c3613744
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-18 23:00:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  conventional_produce = [["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]]
+  [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
   
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
@@ -33,17 +33,14 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  conventional_produce = [["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]]
-  conventional_produce[1][1]
+  matrix[row][column]
   
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
 end
 
 def matrix_update(matrix, row, column, new_value)
-  fruit = [[ "Cherry", "Apple", "Banana"], ["Mango", "Grape", "Orange"], ["Peach", "Kiwi", "Papaya"]]
-  fruit[1][1]
-  fruit
+  
   
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
